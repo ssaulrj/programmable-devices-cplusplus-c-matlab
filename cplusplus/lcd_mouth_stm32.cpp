72aeb9c616561edd9f00b7081707075176a9d357
Saúl Juárez <59984185+ssaulrj@users.noreply.github.com>
2020-04-08 23:01:25 -0500

    Create lcd_mouth_stm32.cpp

---
+++ cplusplus/lcd_mouth_stm32.cpp
@@ -0,0 +1,21 @@
+//Boca abierta
+const char character[] = {14,29,31,28,24,28,31,14};
+
+void CustomChar(char pos_row, char pos_char) {
+  char i;
+    Lcd_Cmd(64);
+    for (i = 0; i<=7; i++) Lcd_Chr_CP(character[i]);
+    Lcd_Cmd(_LCD_RETURN_HOME);
+    Lcd_Chr(pos_row, pos_char, 0);
+}
+
+//Cerrada
+const char character[] = {14,29,31,31,24,31,31,14};
+
+void CustomChar(char pos_row, char pos_char) {
+  char i;
+    Lcd_Cmd(64);
+    for (i = 0; i<=7; i++) Lcd_Chr_CP(character[i]);
+    Lcd_Cmd(_LCD_RETURN_HOME);
+    Lcd_Chr(pos_row, pos_char, 0);
+}
